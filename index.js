88350dd4dd04f18b3646884a66ceeaa4a3321b1d
Sergei Pastushenko <sergei6463597314@gmail.com>
2025-01-25 22:21:03 -0500

    innitial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleMobileMenu(event) {
+    event.preventDefault(); // предотвращает переход по ссылке
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("active");
+}
