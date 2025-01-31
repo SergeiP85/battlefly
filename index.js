a5ad8f00532adef6ce3431d22ffdea628eb3febe
Sergei Pastushenko <sergei6463597314@gmail.com>
2025-01-30 22:50:57 -0500

    burger up

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function toggleMobileMenu(event) {
     event.preventDefault(); // предотвращает переход по ссылке
+    
     const menu = document.getElementById("menu");
+    const icon = document.querySelector('.burger-icon');
+
     menu.classList.toggle("active");
+    icon.classList.toggle("rotated");
 }
+
+
 
 
 
